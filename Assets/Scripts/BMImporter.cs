87a0112c93e16a8d12463377c3d806641b492377
markalmax <markalmax782@gmail.com>
2026-02-11 23:32:03 +0200

    BM Importing

---
+++ Assets/Scripts/BMImporter.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BMImporter : MonoBehaviour
+{
+    public TextAsset OSUFile;
+    public string[] OSULines = new string[0];
+    public Dictionary<string, string> metadata = new Dictionary<string, string>();
+    void Start()
+    {
+        OSULines = OSUFile.text.Split('\n');
+        for(int i = 0; i < OSULines.Length; i++)
+        {
+            if (OSULines[i].Contains(":"))
+            {
+                string[] split = OSULines[i].Split(':');
+                metadata.Add(split[0].Trim(), split[1].Trim());
+                if(split[0].Trim()=="SliderTickRate")break;
+            }
+        }
+        Debug.Log(metadata["Title"] + " " + metadata["Artist"] + " " + metadata["Creator"] + " " + metadata["Version"]);
+    }
+    void Update()
+    {
+        
+    }
+}
